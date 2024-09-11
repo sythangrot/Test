6e4fd83f37e709991277ac29c26f9ba400dfcb5f
sythangrot <167685054+sythangrot@users.noreply.github.com>
2024-09-11 15:50:34 +0700

    Upload những thứ ban đầu
    Done CV1

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 0;
+    while (x <= 10)
+    {
+
+        cout << x << endl;
+        if (x == 5)
+        {
+            x++; 
+        }
+        x++;
+    }
+}
